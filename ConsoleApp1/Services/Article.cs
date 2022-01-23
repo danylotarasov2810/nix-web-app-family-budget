8d5bcf2f151b4de0bf147583d1ffb07a1757e93a
Данил Тарасов <daniltarasov2810@gmail.com>
2022-01-23 20:44:18 +0200

    add services

---
+++ ConsoleApp1/Services/Article.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.DTO;
+using BL.Interfaces;
+
+namespace BL.Services
+{
+    public class Article : IArticle
+    {
+        public void AddArticle(ArticlecostsDTO articlecostsDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteArticle(ArticlecostsDTO articlecostsDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditArticle(ArticleearnsDTO articleearnsDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApp1/Services/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.DTO;
+using BL.Interfaces;
+
+namespace BL.Services
+{
+    public class User : IUser
+    {
+        public void Adduser(UsersDTO usersDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deleteuser(UsersDTO usersDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edituser(UsersDTO usersDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
